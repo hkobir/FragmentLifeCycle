5c891c6a85d76717bcd4a641b4da13c2b3e37331
hkobir <hkobir10@gmail.com>
2019-10-30 20:20:22 +0600

    add fragment and load it machanism

--- app/src/main/java/com/hk/fragmentlifecycle/MainActivity.java
+++ app/src/main/java/com/hk/fragmentlifecycle/MainActivity.java
@@ -1,13 +1,61 @@
 package com.hk.fragmentlifecycle;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+private final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //load the fragment using FragmentManager
+        BlankFragment fragment = new BlankFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction =manager.beginTransaction();
+        transaction.add(R.id.container,fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+        Log.i(TAG,"onAttachFragment");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
     }
 }
